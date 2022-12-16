d2f0c175ab87f43cf1758634e9500031bb1dd36b
farhan-sadiq-mahi <fsmahi100@gmail.com>
2022-12-16 09:13:07 +0600

    img added

--- src/Components/FooterNav.js
+++ src/Components/FooterNav.js
@@ -2,6 +2,10 @@ import { format } from 'date-fns';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Chaffle from 'chaffle';
+import img1 from '../assets/footerImg/man.png'
+import img2 from '../assets/footerImg/project.png'
+import img3 from '../assets/footerImg/skills.png'
+import img4 from '../assets/footerImg/contact.png'
 
 const FooterNav = () => {
     const today = new Date();
@@ -41,23 +45,24 @@ const FooterNav = () => {
         <div className='border-2 bg-gray-900 bg-opacity-90 border-pink-500 fixed bottom-0 w-full flex items-center justify-around'>
             <div className='flex items-center gap-2'>
                 <Link to='/' className='flex justify-center items-center gap-1 onCursor border-pink-500 border-2 px-5 py-2 text-pink-500'>
-                    <img src="http://cypherwebs.xyz/images/man.png" alt="profile" className='w-6' />
+                    <img src={img1}
+                        alt="profile" className='w-6' />
                     <h1 className='text-sm hidden md:block' data-chaffle="en">Profile.exe</h1>
                 </Link>
                 <Link to='/projects' className='flex justify-center items-center
             gap-1 onCursor border-pink-500 border-2 px-5 py-2 text-pink-500'>
-                    <img src="http://cypherwebs.xyz/images/project.png" alt="Projects" className='w-6' />
+                    <img src={img2} alt="Projects" className='w-6' />
                     <h1 className='text-sm hidden md:block' data-chaffle="en">Projects.bat</h1>
 
                 </Link>
                 <Link to='/skills' className='flex justify-center items-center
             gap-1 onCursor border-pink-500 border-2 px-5 py-2 text-pink-500'>
-                    <img src="http://cypherwebs.xyz/images/skills.png" alt="Projects" className='w-6' />
+                    <img src={img3} alt="Projects" className='w-6' />
                     <h1 className='text-sm hidden md:block' data-chaffle="en">Skills.pdf</h1>
                 </Link>
                 <Link to='/contacts' className='flex justify-center items-center
             gap-1 onCursor border-pink-500 border-2 px-5 py-2 text-pink-500'>
-                    <img src="http://cypherwebs.xyz/images/contact.png" alt="Projects" className='w-6' />
+                    <img src={img4} alt="Projects" className='w-6' />
                     <h1 className='text-sm hidden md:block' data-chaffle="en">Contact.zip</h1>
                 </Link>
             </div>
